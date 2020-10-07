ac2a69b3f290c0940a78c20e5d61966e6956ea48
Christopher Corvo <christophercorvo@gmail.com>
2020-10-07 08:45:40 -0400

    first push

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -2,13 +2,25 @@ import React from 'react';
 import Comment from './Comment';
 import './Comments.css';
 
-const Comments = props => {
-  // 🔥 Make sure the parent of Comments is passing the right props!
-  const { comments } = props;
+//posts.js is the parent 
 
+const Comments = props => {
+ 
+  // 🔥 Make sure the parent of Comments is passing the right props!
+  const { comments } = props;//dumby data
+  console.log(comments)
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map(commentIndex => {
+          const newArray = commentIndex.comments
+        
+          newArray.forEach(element => {
+            console.log(element)
+            // return <Comment comment = {element}/> // need to get to the individul comments 
+          });
+        
+      })}
     </div>
   );
 };
--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -3,10 +3,13 @@ import Comments from '../Comments/Comments';
 import LikeSection from './LikeSection';
 import PostHeader from './PostHeader';
 
+//posts.js is the parent
+
 const Post = props => {
+
   // 🔥 Make sure the parent of Post is passing the right props!
   const { post, likePost } = props;
-
+    
   return (
     <div className='post-border'>
       <PostHeader
@@ -21,9 +24,9 @@ const Post = props => {
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
-      {/* Comments also wants its props! */}
-      <Comments />
+      <LikeSection likePost={() => likePost(post.id)} numberOfLikes = {post.numberOfLikes}/>
+      {/* Comments also wants its props! line 33 = dummbdata[1] and dumbydata[2]*/}
+      <Comments comments = {post}/> 
     </div>
   );
 };
